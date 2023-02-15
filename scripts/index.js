3a12c857bdba0936ffc618efdd1236d023f16ec0
Marina Khusainova <marina.iwtar@yandex.ru>
2023-02-15 09:58:07 +0500

    Второе исправление ревью

--- scripts/index.js
+++ scripts/index.js
@@ -7,17 +7,17 @@ let userAboutMy = document.querySelector('.profile-info__paragraph');
 let nameInput = document.querySelector('.popup__input_type_name');
 let aboutMyInput = document.querySelector('.popup__input_type_about-my');
 
-function openModal(evt) {
+function openModal(evt) { //функция открытие модального окна
   editPopup.classList.add("popup_opened");
+  nameInput.value = userName.textContent;
+  aboutMyInput.value = userAboutMy.textContent;
 }
 
-function closeModal(evt) {
+function closeModal(evt) { //функция закрытия модального окна
   editPopup.classList.remove("popup_opened");
 }
-nameInput.value = userName.textContent;
-aboutMyInput.value = userAboutMy.textContent;
 
-function formSubmitHandler (evt) {
+function formSubmitHandler (evt) { //функция внесение изменений в строки профиля из формы модального окна
 	evt.preventDefault(); 
 
   userName.textContent = nameInput.value;
@@ -25,6 +25,6 @@ function formSubmitHandler (evt) {
   
   closeModal();
 }
-popupClose.addEventListener("click", closeModal);
+popupClose.addEventListener("click", closeModal); 
 editButton.addEventListener("click", openModal);
 formElement.addEventListener('submit', formSubmitHandler);
